c9cb21bf710384126b3d85bfdd556d844de948a1
Abd elhakim Zayan <abdozayan12@gmailcom>
2023-06-20 18:36:37 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,23 +1,6 @@
 import './style.css';
-import tasks, { submit, listContainer } from './modules/tasks.js';
-import TaskStatus from './modules/Status.js';
+import students from './modules/students.js';
 
-window.onload = tasks.displayTasks();
-TaskStatus.updateStatus();
-TaskStatus.clearCompleted();
+const studentsScores = document.querySelector('.scores-list');
 
-const refersh = document.querySelector('.refresh');
-refersh.addEventListener('click', () => {
-  window.location.reload();
-});
-
-submit.addEventListener('keypress', (e) => {
-  if (e.key === 'Enter') {
-    e.preventDefault();
-    tasks.addTask();
-    submit.value = '';
-    listContainer.innerHTML = '';
-    tasks.displayTasks();
-    TaskStatus.updateStatus();
-  }
-});
+studentsScores.innerHTML = students.map((student) => `<tr><td>${student.name}: ${student.score}</td></tr>`).join('');
