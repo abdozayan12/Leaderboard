e47bfb50b44e9b82897ad99ce0983cafbc2c4ca2
Abd elhakim Zayan <abdozayan12@gmailcom>
2023-06-20 17:52:36 +0300

    linters added

--- src/__mocks__/editTask.js
+++
@@ -1,7 +0,0 @@
-const editTask = () => {
-  // Implement the editing task
-  const editTaskInput = document.querySelector('.tasks-container li');
-
-  editTaskInput.innerHTML = 'Matching test';
-};
-export default editTask;
