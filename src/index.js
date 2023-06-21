82c9ee78a7890351915bad356257e503704fbe57
Abd elhakim Zayan <abdozayan12@gmailcom>
2023-06-21 12:53:24 +0300

    add api.js & board.js files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,37 @@
 import './style.css';
-import students from './modules/students.js';
+import { createScore, getScore } from './modules/api.js';
+import Board from './modules/Board.js';
 
-const studentsScores = document.querySelector('.scores-list');
+const submit = document.getElementById('submit');
 
-studentsScores.innerHTML = students.map((student) => `<tr><td>${student.name}: ${student.score}</td></tr>`).join('');
+submit.addEventListener('click', (e) => {
+    e.preventDefault();
+    const student = document.getElementById('student');
+    const score = document.getElementById('score');
+  
+    createScore(student.value, score.value);
+    student.value = '';
+    score.value = '';
+  });
+  
+  const students = new Board();
+  const print = () => {
+    const result = getScore();
+    result.then((res) => {
+        students.data = res.result;
+      const studentsScores = document.querySelector('.scores-list');
+  
+      // sorted the array descending order
+      const sortedScores = students.data.sort((a, b) => b.score - a.score);
+  
+      studentsScores.innerHTML = sortedScores.map((student) => `<tr><td>${student.user}: ${student.score}</td></tr>`).join('');
+    });
+  };
+  
+  const refreshButton = document.getElementById('refBtn');
+  
+  refreshButton.addEventListener('click', () => {
+    print();
+  });
+  
+  window.onload = print();
